6973f1469cf330d83bbaf3b56c92261b51453908
Felipe Tozadori Silva <ftozadori@gmail.com>
2025-09-09 03:18:03 -0300

    MinimalApi
    Criando uma Minimial API usando Swagger e EntityFramework

---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("Clientes")); 
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+
+app.MapGet("/clientes", async (AppDbContext dbContext) => await dbContext.Clientes.ToListAsync());
+
+app.MapGet("/clientes/{id}", async (int id, AppDbContext dbContext) =>
+    await dbContext.Clientes.FirstOrDefaultAsync(a => a.Id == id));
+
+app.MapPost("/Clientes", async (Cliente cliente, AppDbContext dbContext) =>
+{
+    dbContext.Clientes.Add(cliente);
+    await dbContext.SaveChangesAsync();
+    return cliente;
+});
+
+app.MapPut("/Clientes/{id}", async (int id, Cliente cliente, AppDbContext dbContext) =>
+{
+    var clienteExistente = await dbContext.Clientes.FirstOrDefaultAsync(c => c.Id == id);
+    if (clienteExistente == null)
+    {
+        return Results.NotFound();
+    }
+
+    clienteExistente.Nome = cliente.Nome;
+    await dbContext.SaveChangesAsync();
+    return Results.Ok(clienteExistente);
+});
+
+app.MapDelete("/Clientes/{id}", async (int id, AppDbContext dbContext) =>
+{
+    var cliente = await dbContext.Clientes.FirstOrDefaultAsync(a => a.Id == id);
+
+    if(cliente != null)
+    {
+        dbContext.Clientes.Remove(cliente);
+        await dbContext.SaveChangesAsync();
+    }
+    return;
+});
+
+app.UseSwaggerUI();
+
+app.Run();
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+}
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<Cliente> Clientes { get; set; }
+}
